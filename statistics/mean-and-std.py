f39d3ca124e25348a3f64726ec304adc6aacafc0
asanka-code <asanka.code@gmail.com>
2016-08-26 12:49:10 +0530

    histogram of sine waveform

--- statistics/mean-and-std.py
+++ statistics/mean-and-std.py
@@ -10,6 +10,14 @@ sample = 8000
 x = np.arange(sample)
 y = np.sin(2 * np.pi * f * x / Fs)
 
+# plotting the signal waveform
+plt.hist(y, normed=True)
+plt.title("Histogram of sine wave")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.grid(True)
+plt.show()
+
 for i in range(sample):
     y[i] = y[i]*gauss(0,1)
 
@@ -18,10 +26,21 @@ print 'mean:' ,np.mean(y)
 print 'std:', np.std(y)
 print 'SNR:', np.mean(y)/np.std(y)
 
-# plotting the signal
+# plotting the signal waveform
+plt.title('Waveform of gaussian signal')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.ylim(-5, 5)
+plt.grid(True)
 plt.plot(x, y)
 plt.show()
+
+# plotting the histogram
+plt.hist(y, normed=True)
+plt.title("Histogram of gaussian signal")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.grid(True)
+plt.show()
+
 
